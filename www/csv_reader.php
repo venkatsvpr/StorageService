86d4e0e230391ce62fcc3e671e2b03de08f81e38
vikram <vikramsinghh11@gmail.com>
2018-12-04 18:01:42 -0500

    added reader for new csv

--- www/csv_reader.php
+++ www/csv_reader.php
@@ -3,8 +3,32 @@
   $csv_file_loc = "/tmp/Localization.csv";
   $csv_file_sync = "/tmp/SyncFetch.csv";
   $csv_file_async = "/tmp/AsyncFetch.csv";
+  $csv_trajectory = "/tmp/trajectory.csv";
 
-  if($_REQUEST["time-localization"]){
+
+
+  if(isset($_REQUEST["trajectory"]) && $_REQUEST["trajectory"]){
+    $labels = array();
+    $time = array();
+    $file = fopen($csv_trajectory, 'r');
+    while (($line = fgetcsv($file)) !== FALSE) {
+      $first[] = $line[0];
+      $second[] = $line[1];
+      $third[] = $line[2];
+    }
+
+    $map = array();
+    $map['x'] = $first;
+    $map['y'] = $second;
+    $map['type'] = $third;
+    fclose($file);
+
+    $response_json = json_encode($map);
+    echo $response_json;
+  }
+
+
+  if(isset($_REQUEST["time-localization"]) && $_REQUEST["time-localization"]){
     $labels = array();
     $time = array();
     $file = fopen($csv_file_loc, 'r');
@@ -21,7 +45,7 @@
     echo $response_json;
   }
 
-  if($_REQUEST["time-sync"]){
+  if(isset($_REQUEST["time-sync"]) && $_REQUEST["time-sync"] ){
     $labels = array();
     $time = array();
     $file = fopen($csv_file_sync, 'r');
@@ -38,7 +62,7 @@
     echo $response_json;
   }
 
-  if($_REQUEST["time-async"]){
+  if(isset($_REQUEST["time-async"]) && $_REQUEST["time-async"]){
     $labels = array();
     $time = array();
     $file = fopen($csv_file_async, 'r');
