c09b42a30a76fdbfa04f274490cd056975b0a954
Sandeep Kumar <skumar28@buffalo.edu>
2018-12-13 07:27:09 -0800

    Name change From Storage Service to Caching Service

--- www/index.php
+++ www/index.php
@@ -37,7 +37,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Storage Service</a>
+        <a class="navbar-brand" href="#">Caching Service for AR Applications</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
